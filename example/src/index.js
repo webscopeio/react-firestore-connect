c633b2f491b4dea3d621cac82ec692cfd4fcb0fe
Oliver Rýdzi <orydzi@gmail.com>
2018-09-21 20:21:20 +0200

    Initialize eslint and flow on the example project, added firebase project into it

--- example/src/index.js
+++ example/src/index.js
@@ -3,5 +3,21 @@ import ReactDOM from 'react-dom'
 
 import './index.css'
 import App from './App'
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyBEpuBdyJ7ex7IbmxcrxTeG5wcAIlmQMoc',
+  authDomain: 'connect-firestore.firebaseapp.com',
+  databaseURL: 'https://connect-firestore.firebaseio.com',
+  projectId: 'connect-firestore',
+  storageBucket: 'connect-firestore.appspot.com',
+  messagingSenderId: '484397672960'
+}
+firebase.initializeApp(config)
+
+const firestore = firebase.firestore()
+const settings = { timestampsInSnapshots: true }
+firestore.settings(settings)
 
 ReactDOM.render(<App />, document.getElementById('root'))
